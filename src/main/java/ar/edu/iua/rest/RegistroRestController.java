12aa188417574b812dd51c730c5708119edd03e5
sofiapfeigin <69279758+sofiapfeigin@users.noreply.github.com>
2021-06-16 18:15:04 -0300

    Update RegistroRestController.java

--- src/main/java/ar/edu/iua/rest/RegistroRestController.java
+++ src/main/java/ar/edu/iua/rest/RegistroRestController.java
@@ -39,6 +39,7 @@ public class RegistroRestController {
 			responseHeaders.set("location", Constantes.URL_REGISTROS + "/" + registro.getId());
 			return new ResponseEntity<String>(responseHeaders, HttpStatus.CREATED);
 		} catch (BusinessException e) {
+			System.out.print("estoy en rest "+e);
 			return new ResponseEntity<String>(HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
